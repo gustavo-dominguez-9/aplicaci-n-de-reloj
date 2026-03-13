9ca2b2912fa1372756167934f2d1d79bb5099d77
gustavo-dominguez-9 <gustavo.dominguez@iest.edu.mx>
2026-03-13 08:34:10 -0600

    actualizacion
    se agrego una opción para cambiar el color del reloj y bases para una nueva sección de configuración

--- aplicacion,reloj/aplicacion,reloj/Form1.Designer.vb
+++ aplicacion,reloj/aplicacion,reloj/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,7 +20,7 @@ Partial Class Form1
     'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
     'Se puede modificar usando el Diseñador de Windows Forms.  
     'No lo modifique con el editor de código.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
         Me.hora = New System.Windows.Forms.Label()
@@ -29,6 +29,15 @@ Partial Class Form1
         Me.dia = New System.Windows.Forms.Label()
         Me.fecha = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.panel1 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.conf = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'hora
@@ -38,7 +47,7 @@ Partial Class Form1
         Me.hora.AutoSize = True
         Me.hora.Font = New System.Drawing.Font("Proxy 9", 90.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.hora.ForeColor = System.Drawing.Color.Lime
-        Me.hora.Location = New System.Drawing.Point(105, 65)
+        Me.hora.Location = New System.Drawing.Point(139, 65)
         Me.hora.Name = "hora"
         Me.hora.Size = New System.Drawing.Size(208, 146)
         Me.hora.TabIndex = 0
@@ -51,7 +60,7 @@ Partial Class Form1
         Me.segundo.AutoSize = True
         Me.segundo.Font = New System.Drawing.Font("Proxy 9", 90.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.segundo.ForeColor = System.Drawing.Color.Lime
-        Me.segundo.Location = New System.Drawing.Point(417, 65)
+        Me.segundo.Location = New System.Drawing.Point(394, 65)
         Me.segundo.Name = "segundo"
         Me.segundo.Size = New System.Drawing.Size(208, 146)
         Me.segundo.TabIndex = 1
@@ -62,13 +71,13 @@ Partial Class Form1
         Me.Label3.AccessibleDescription = "group"
         Me.Label3.AccessibleName = "group"
         Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.Label3.Font = New System.Drawing.Font("Proxy 9", 90.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label3.ForeColor = System.Drawing.Color.Lime
-        Me.Label3.Location = New System.Drawing.Point(319, 65)
+        Me.Label3.Location = New System.Drawing.Point(338, 65)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(92, 146)
+        Me.Label3.Size = New System.Drawing.Size(0, 146)
         Me.Label3.TabIndex = 2
-        Me.Label3.Text = ":"
         '
         'dia
         '
@@ -99,19 +108,92 @@ Partial Class Form1
         'Timer1
         '
         '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.ForeColor = System.Drawing.Color.Lime
+        Me.Button1.Location = New System.Drawing.Point(60, 356)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(59, 31)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "💡"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Timer2
+        '
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.aplicacion_reloj.My.Resources.Resources.icon6
+        Me.PictureBox1.Location = New System.Drawing.Point(577, 329)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(118, 58)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'panel1
+        '
+        Me.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.panel1.Controls.Add(Me.conf)
+        Me.panel1.Controls.Add(Me.Button2)
+        Me.panel1.Location = New System.Drawing.Point(25, 12)
+        Me.panel1.Name = "panel1"
+        Me.panel1.Size = New System.Drawing.Size(243, 286)
+        Me.panel1.TabIndex = 7
+        Me.panel1.Visible = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(16, 79)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(68, 56)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'conf
+        '
+        Me.conf.AutoSize = True
+        Me.conf.BackColor = System.Drawing.SystemColors.Control
+        Me.conf.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.conf.Location = New System.Drawing.Point(45, 17)
+        Me.conf.Name = "conf"
+        Me.conf.Size = New System.Drawing.Size(144, 24)
+        Me.conf.TabIndex = 4
+        Me.conf.Text = "configuraciones"
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button3.ForeColor = System.Drawing.Color.Lime
+        Me.Button3.Location = New System.Drawing.Point(136, 356)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(59, 31)
+        Me.Button3.TabIndex = 8
+        Me.Button3.Text = "⚙"
+        Me.Button3.UseVisualStyleBackColor = False
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.ClientSize = New System.Drawing.Size(707, 395)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.panel1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.fecha)
         Me.Controls.Add(Me.dia)
-        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.segundo)
         Me.Controls.Add(Me.hora)
         Me.Name = "Form1"
         Me.Text = "reloj digital"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.panel1.ResumeLayout(False)
+        Me.panel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -123,4 +205,11 @@ Partial Class Form1
     Friend WithEvents dia As Label
     Friend WithEvents fecha As Label
     Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents panel1 As Panel
+    Friend WithEvents conf As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
 End Class
--- aplicacion,reloj/aplicacion,reloj/Form1.vb
+++ aplicacion,reloj/aplicacion,reloj/Form1.vb
@@ -1,7 +1,8 @@
 ﻿Public Class Form1
+    Dim contador As Integer = 0
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Timer1.Start()
-
+        Timer2.Start()
     End Sub
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -27,6 +28,92 @@
     End Sub
 
     Private Sub hora_Click(sender As Object, e As EventArgs) Handles hora.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        contador += 1
+
+        If contador = 1 Then
+            hora.ForeColor = Color.Red
+            Label3.ForeColor = Color.Red
+            segundo.ForeColor = Color.Red
+            fecha.ForeColor = Color.Red
+            dia.ForeColor = Color.Red
+            Button1.ForeColor = Color.Red
+            Button3.ForeColor = Color.Red
+        ElseIf contador = 2 Then
+            hora.ForeColor = Color.Fuchsia
+            Label3.ForeColor = Color.Fuchsia
+            segundo.ForeColor = Color.Fuchsia
+            fecha.ForeColor = Color.Fuchsia
+            dia.ForeColor = Color.Fuchsia
+            Button1.ForeColor = Color.Fuchsia
+            Button3.ForeColor = Color.Fuchsia
+        ElseIf contador = 3 Then
+            hora.ForeColor = Color.Yellow
+            Label3.ForeColor = Color.Yellow
+            segundo.ForeColor = Color.Yellow
+            fecha.ForeColor = Color.Yellow
+            dia.ForeColor = Color.Yellow
+            Button1.ForeColor = Color.Yellow
+            Button3.ForeColor = Color.Yellow
+        ElseIf contador = 5 Then
+            hora.ForeColor = Color.Lime
+            Label3.ForeColor = Color.Lime
+            segundo.ForeColor = Color.Lime
+            fecha.ForeColor = Color.Lime
+            dia.ForeColor = Color.Lime
+            Button1.ForeColor = Color.Lime
+            Button3.ForeColor = Color.Lime
+        ElseIf contador = 4 Then
+            hora.ForeColor = Color.Aqua
+            Label3.ForeColor = Color.Aqua
+            segundo.ForeColor = Color.Aqua
+            fecha.ForeColor = Color.Aqua
+            dia.ForeColor = Color.Aqua
+            Button1.ForeColor = Color.Aqua
+            Button3.ForeColor = Color.Aqua
+
+
+        ElseIf contador = 6 Then
+            contador = 0
+
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label3.Text = ":" Then
+            Label3.Text = ""
+        ElseIf label3.text = "" Then
+            Label3.Text = ":"
+        End If
+    End Sub
+
+    Private Sub fecha_Click(sender As Object, e As EventArgs) Handles fecha.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If panel1.Visible = False Then
+            panel1.Visible = True
+        ElseIf panel1.visible = True Then
+            panel1.Visible = False
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If panel1.Visible = False Then
+            panel1.Visible = True
+        ElseIf panel1.Visible = True Then
+            panel1.Visible = False
+
+        End If
 
     End Sub
 End Class
