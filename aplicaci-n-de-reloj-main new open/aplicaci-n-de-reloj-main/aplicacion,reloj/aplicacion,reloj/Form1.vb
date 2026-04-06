a9bc6a2102f3344590dc1391432a16b01fc4414a
gustavo-dominguez-9 <gustavo.dominguez@iest.edu.mx>
2026-04-06 08:40:43 -0600

    se agregan bases para el cronometro

---
+++ aplicaci-n-de-reloj-main new open/aplicaci-n-de-reloj-main/aplicacion,reloj/aplicacion,reloj/Form1.vb
@@ -0,0 +1,214 @@
+﻿Public Class Form1
+    Dim contador As Integer = 0
+    Dim formato As Boolean = True
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'iniciar los timers
+        Timer1.Start()
+        Timer2.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'crea una variable y asigna un valor, el valor es la fecha del dia de hoy convertido a string en el formato de dia, mes y año 
+        Dim Date_o = DateTime.Now.ToString("dd 'de' MMMM 'del' yyyy")
+        Dim Day_o = DateTime.Now.ToString("dddd")
+        Dim Time_o As String = ""
+
+
+        ' este es el fromato de 24hrs 
+
+        If formato = True Then
+            Time_o = DateTime.Now.ToString("HH:mm:ss")
+        ElseIf formato = False Then
+            Time_o = DateTime.Now.ToString("hh:mm:ss")
+        End If
+
+        Dim Time_mod = Time_o.Split(":")
+        minutos.text = Time_mod(1)
+        hora.Text = Time_mod(0)
+        segundos.Text = Time_mod(2)
+        dia.Text = Day_o
+        fecha.Text = Date_o
+
+    End Sub
+
+    Private Sub segundo_Click(sender As Object, e As EventArgs) Handles minutos.Click
+
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+
+    Private Sub hora_Click(sender As Object, e As EventArgs) Handles hora.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        contador += 1
+
+        If contador = 1 Then
+            hora.ForeColor = Color.Red
+            Label3.ForeColor = Color.Red
+            minutos.ForeColor = Color.Red
+            fecha.ForeColor = Color.Red
+            dia.ForeColor = Color.Red
+            Button1.ForeColor = Color.Red
+            Button3.ForeColor = Color.Red
+            conf.ForeColor = Color.Red
+            Button2.ForeColor = Color.Red
+        ElseIf contador = 2 Then
+            hora.ForeColor = Color.Fuchsia
+            Label3.ForeColor = Color.Fuchsia
+            minutos.ForeColor = Color.Fuchsia
+            fecha.ForeColor = Color.Fuchsia
+            dia.ForeColor = Color.Fuchsia
+            Button1.ForeColor = Color.Fuchsia
+            Button3.ForeColor = Color.Fuchsia
+            conf.ForeColor = Color.Fuchsia
+            Button2.ForeColor = Color.Fuchsia
+        ElseIf contador = 3 Then
+            hora.ForeColor = Color.Yellow
+            Label3.ForeColor = Color.Yellow
+            minutos.ForeColor = Color.Yellow
+            fecha.ForeColor = Color.Yellow
+            dia.ForeColor = Color.Yellow
+            Button1.ForeColor = Color.Yellow
+            Button3.ForeColor = Color.Yellow
+            conf.ForeColor = Color.Yellow
+            Button2.ForeColor = Color.Yellow
+        ElseIf contador = 5 Then
+            hora.ForeColor = Color.Lime
+            Label3.ForeColor = Color.Lime
+            minutos.ForeColor = Color.Lime
+            fecha.ForeColor = Color.Lime
+            dia.ForeColor = Color.Lime
+            Button1.ForeColor = Color.Lime
+            Button3.ForeColor = Color.Lime
+            conf.ForeColor = Color.Lime
+            Button2.ForeColor = Color.Lime
+        ElseIf contador = 4 Then
+            hora.ForeColor = Color.Aqua
+            Label3.ForeColor = Color.Aqua
+            minutos.ForeColor = Color.Aqua
+            fecha.ForeColor = Color.Aqua
+            dia.ForeColor = Color.Aqua
+            Button1.ForeColor = Color.Aqua
+            Button3.ForeColor = Color.Aqua
+            conf.ForeColor = Color.Aqua
+            Button2.ForeColor = Color.Aqua
+
+
+        ElseIf contador = 6 Then
+            contador = 0
+
+        End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        If Label3.Text = ":" Then
+            Label3.Text = ""
+        ElseIf Label3.Text = "" Then
+            Label3.Text = ":"
+        End If
+    End Sub
+
+    Private Sub fecha_Click(sender As Object, e As EventArgs) Handles fecha.Click
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If panel1.Visible = False Then
+            panel1.Visible = True
+        ElseIf panel1.Visible = True Then
+            panel1.Visible = False
+
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If panel1.Visible = False Then
+            panel1.Visible = True
+        ElseIf panel1.Visible = True Then
+            panel1.Visible = False
+
+        End If
+
+    End Sub
+
+    Private Sub panel1_Paint(sender As Object, e As PaintEventArgs) Handles panel1.Paint
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim color_texto As String = ComboBox1.SelectedItem.ToString()
+        If color_texto = "Blanco" Then
+            hora.ForeColor = Color.White
+            Label3.ForeColor = Color.White
+            minutos.ForeColor = Color.White
+            fecha.ForeColor = Color.White
+            dia.ForeColor = Color.White
+            Button1.ForeColor = Color.White
+            Button3.ForeColor = Color.White
+            conf.ForeColor = Color.White
+            Button2.ForeColor = Color.White
+        ElseIf color_texto = "Rojo" Then
+            hora.ForeColor = Color.Red
+            Label3.ForeColor = Color.Red
+            minutos.ForeColor = Color.Red
+            fecha.ForeColor = Color.Red
+            dia.ForeColor = Color.Red
+            Button1.ForeColor = Color.Red
+            Button3.ForeColor = Color.Red
+            conf.ForeColor = Color.Red
+            Button2.ForeColor = Color.Red
+        ElseIf color_texto = "Amarillo" Then
+            hora.ForeColor = Color.Yellow
+            Label3.ForeColor = Color.Yellow
+            minutos.ForeColor = Color.Yellow
+            fecha.ForeColor = Color.Yellow
+            dia.ForeColor = Color.Yellow
+            Button1.ForeColor = Color.Yellow
+            Button3.ForeColor = Color.Yellow
+            conf.ForeColor = Color.Yellow
+            Button2.ForeColor = Color.Yellow
+        ElseIf color_texto = "Verde" Then
+            hora.ForeColor = Color.Lime
+            Label3.ForeColor = Color.Lime
+            minutos.ForeColor = Color.Lime
+            fecha.ForeColor = Color.Lime
+            dia.ForeColor = Color.Lime
+            Button1.ForeColor = Color.Lime
+            Button3.ForeColor = Color.Lime
+            conf.ForeColor = Color.Lime
+            Button2.ForeColor = Color.Lime
+        ElseIf color_texto = "Azul" Then
+            hora.ForeColor = Color.Aqua
+            Label3.ForeColor = Color.Aqua
+            minutos.ForeColor = Color.Aqua
+            fecha.ForeColor = Color.Aqua
+            dia.ForeColor = Color.Aqua
+            Button1.ForeColor = Color.Aqua
+            Button3.ForeColor = Color.Aqua
+            conf.ForeColor = Color.Aqua
+            Button2.ForeColor = Color.Aqua
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If formato = True Then
+            Button2.Text = "12h"
+            formato = False
+        ElseIf formato = False Then
+            Button2.Text = "24h"
+            formato = True
+        End If
+    End Sub
+
+    Private Sub segundos_Click(sender As Object, e As EventArgs) Handles segundos.Click
+
+    End Sub
+End Class
